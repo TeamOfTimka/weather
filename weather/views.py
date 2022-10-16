45e1b6e399921501c18f5361003c38e2deb90372
TeamOfTimka <lozhkintima112@gmail.com>
2022-10-16 12:37:42 +0300

    added functionality

--- weather/views.py
+++ weather/views.py
@@ -1,4 +1,19 @@
 from django.shortcuts import render
+import requests
 
 def index(request):
-    return render(request, 'weather/index.html')
+    appid = "0efd99ccc7371294a461e8f9b7d74893"
+    url = 'https://samples.openweathermap.org/data/2.5/weather?q={}&appid=' + appid
+    
+    city = 'London'
+
+    res = requests.get(url.format(city)).json()
+    city_info = {
+        'city': city,
+        'temp': int(res['main']['temp'])-273,
+        'icon': res['weather'][0]['icon']
+    }
+
+    context = {'info': city_info}
+
+    return render(request, 'weather/index.html', context)
